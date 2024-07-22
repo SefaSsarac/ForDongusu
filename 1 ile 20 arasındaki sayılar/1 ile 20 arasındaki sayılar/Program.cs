2c3d4d72fd54369b1b564c87e444db95292931bb
Sefa Saraç <srcsfysf@gmail.com>
2024-07-22 15:50:01 +0300

    Add files via upload

---
+++ 1 ile 20 arasındaki sayılar/1 ile 20 arasındaki sayılar/Program.cs
@@ -0,0 +1,4 @@
+﻿for (int i = 2; i < 20; i++)
+{
+    Console.WriteLine($"1 ile 20 arasindaki sayilar: {i} ");
+}
---
+++ 1 ile 20 arasındaki çift sayılar/1 ile 20 arasındaki çift sayılar/Program.cs
@@ -0,0 +1,4 @@
+﻿for (int i = 2; i < 20; i +=2)
+{
+    Console.WriteLine($"1 ile 20 arasindaki cift sayilar: {i} ");
+}
